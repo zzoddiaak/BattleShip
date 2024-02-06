355d7993fe60cc52c152631a8f200c20484cd3a4
Daniil Savko <savko.danik2@gmail.com>
2024-02-06 19:06:28 +0300

    update4

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -150,10 +150,6 @@ class GameBoard {
 
             System.out.print("Ваш ход! Введите координату (например, A3): ");
             String input = scanner.next().toUpperCase();
-            if (input.equals("KL10")) {
-                destroyAllEnemyShips(enemyBoard);
-                return;
-            }
             col = input.charAt(0) - 'A';
             row = Integer.parseInt(input.substring(1)) - 1;
 
@@ -169,7 +165,6 @@ class GameBoard {
         } else {
             System.out.println("Ранил!");
             enemyBoard.enemyBoard[row][col] = 'X';
-            enemyBoard.updateShips(row, col);
         }
     }
 
@@ -193,39 +188,7 @@ class GameBoard {
         } else {
             System.out.println("Ход противника: Ранил!");
             playerBoard.playerBoard[row][col] = 'X';
-            playerBoard.updateShips(row, col);
         }
     }
 
-    public void updateShips(int row, int col) {
-        for (Ship ship : ships) {
-            ship.hit(row, col);
-        }
-
-        if (enemyBoard[row][col] == 'X') {
-            updateSurroundingCells(row, col);
-        }
-    }
-
-    private void updateSurroundingCells(int row, int col) {
-        for (int i = Math.max(0, row - 1); i <= Math.min(SIZE - 1, row + 1); i++) {
-            for (int j = Math.max(0, col - 1); j <= Math.min(SIZE - 1, col + 1); j++) {
-                if (enemyBoard[i][j] == ' ') {
-                    enemyBoard[i][j] = '.';
-                }
-            }
-        }
-    }
-
-    private void destroyAllEnemyShips(GameBoard enemyBoard) {
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                if (enemyBoard.getBoard(false)[i][j] != ' ' && enemyBoard.getBoard(false)[i][j] != '.' && enemyBoard.getBoard(false)[i][j] != 'X') {
-                    enemyBoard.getBoard(false)[i][j] = 'X';
-                    enemyBoard.updateShips(i, j);
-                }
-            }
-        }
-        System.out.println("Победа! Все корабли противника уничтожены!");
-    }
 }
